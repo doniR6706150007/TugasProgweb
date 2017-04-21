8b47298d33a177644782c101738f9d5a444c9129
Novian Nuraldi <novian.nuraldi97@gmail.com>
2017-04-21 18:25:58 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -17,5 +17,30 @@ if($_SESSION['domain']=='guru'){
 }else{
 	$pengguna=ucwords($_SESSION['username']);
 }
+
+<div id="page-heading">
+	<center>
+    <h1>---SELAMAT DATANG---</h1>
+    
+	</center>
+</div>
+<!-- end page-heading -->
+
+<table border="0" width="100%" cellpadding="0" cellspacing="0" id="content-table">
+<tr>
+    <th rowspan="3" class="sized"><img src="images/shared/side_shadowleft.jpg" width="20" height="300" alt="" /></th>
+    <th class="topleft"></th>
+    <td id="tbl-border-top">&nbsp;</td>
+    <th class="topright"></th>
+    <th rowspan="3" class="sized"><img src="images/shared/side_shadowright.jpg" width="20" height="300" alt="" /></th>
+</tr>
+
+<tr>
+    <th class="sized bottomleft"></th>
+    <td id="tbl-border-bottom">&nbsp;</td>
+    <th class="sized bottomright"></th>
+</tr>
+</table>
+
 ?>
 
